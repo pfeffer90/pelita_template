de865dabad98cfb14569aa845956e364b94dc47d
Paul Pfeiffer <pfeifferpaul90@gmail.com>
2019-08-16 13:29:18 +0200

    test for avoid staying on position if enemy is close

--- test_awesome_attacker.py
+++ test_awesome_attacker.py
@@ -1,9 +1,11 @@
-from awesome_attacker import move
 from pelita.utils import setup_test_game
+
+from awesome_attacker import move
+
 
 def test_eat_food():
     # do we eat food when it's available?
-    layout="""
+    layout = """
     ########
     #    0.#
     #.1  EE#
@@ -13,9 +15,29 @@ def test_eat_food():
     next_move, _ = move(bot, None)
     assert next_move == (6, 1)
 
+
+def test_change_position_if_possible_with_enemy_nearby():
+    # move if an enemy is close. The basic attacker could get stuck in place and become prey.
+    layout = """
+    ########
+    #   .E0#
+    #1E    #
+    ########
+    """
+    bot_positions = [(6, 1), (1, 2)]
+    enemy_positions = [(5, 1), (2, 2)]
+    food_positions = [(4, 1)]
+    Numberoftest = 100
+    for i in range(Numberoftest):
+        bot = setup_test_game(layout=layout, is_blue=True, bots=bot_positions, enemy=enemy_positions,
+                              food=food_positions)
+        next_move, _ = move(bot, None)
+        assert next_move != (6, 1)
+
+
 def test_no_kamikaze():
     # do we avoid enemies when they can kill us?
-    layout="""
+    layout = """
     ########
     #    E.#
     #.1  0E#
@@ -25,9 +47,10 @@ def test_no_kamikaze():
     next_move, _ = move(bot, None)
     assert next_move == (4, 2) or next_move == (5, 2)
 
+
 def test_do_not_step_on_enemy():
     # check that we don't step back on an enemy when we are fleeing
-    layout="""
+    layout = """
     ########
     #    E.#
     #.1 #0E#
@@ -36,4 +59,3 @@ def test_do_not_step_on_enemy():
     bot = setup_test_game(layout=layout, is_blue=True)
     next_move, _ = move(bot, None)
     assert next_move == (5, 2)
-
